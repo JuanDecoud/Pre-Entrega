f823038d2e45edfb163bfe37a3e3c5b3c10fa70e
JuanDecoud <juanjo.decoud@gmail.com>
2023-07-06 09:40:34 -0300

    new

--- src/app.js
+++ src/app.js
@@ -7,28 +7,21 @@ import { Server } from 'socket.io';
 import mongoose from 'mongoose'
 import messengerRouter from './routers/messengerRouter.js'
 import messengerModel from './dao/models/messenger.model.js'
-
-
-
+///-------------------------------------------------------
 const app = express ();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./src/public'))
-
-
-/// routers
+/// routers------------------------------------------------
 app.use ('/products' , productRouter)
 app.use ('/carts' , cartsRouter)
 app.use ('/views' , viewRouter)
 app.use('/messenger', messengerRouter)
-//---------------------------------
-/// engine 
+/// engine-------------------------------------------------
 app.engine('handlebars' , handlebars.engine())
 app.set('views','./src/views')
 app.set('view engine' ,'handlebars')
-
-//--------
-
+//runDev----------------------------------------------------
 try{
     await mongoose.connect(`mongodb+srv://juanjodecoud:JJjuanjitus22@cluster0.bpez36c.mongodb.net/proyectocoder`)
     const serverHttp=app.listen(8080 , ()=>console.log("Server Up"))
@@ -42,8 +35,7 @@ try{
         socket.on ('messages',async data=>{
            await messengerModel.create({userMail :data.userMail , messege : data.messege})
            io.emit('updateMessages' , await messengerModel.find().lean())
-        })
-        
+        }) 
     })
 }catch (err) {
   console.log (err.message)
